34774f3732d6fdb1f6511554b0a2defb48da0c54
Alex <Alex@Alex-VAIO.attlocal.net>
2015-02-15 16:57:59 -0600

    Second Commit of this file

--- src/messageservice/MorningGreetingStrategy.java
+++ src/messageservice/MorningGreetingStrategy.java
@@ -5,8 +5,10 @@
  */
 package messageservice;
 
+import java.awt.Component;
 import java.util.Random;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,7 +18,8 @@ public class MorningGreetingStrategy implements MessageStrategy {
 
     String[] greetings = {"Hello", "Good Morning", "Would you like some bacon?",
                             "Enjoy your day", "I am bacon cake", "Sure is cold out today..."};
-    private String newMessage;
+    //private String newMessage;
+    private Component frame;
 
     @Override
     public String returnMessage() {
@@ -28,12 +31,24 @@ public class MorningGreetingStrategy implements MessageStrategy {
 
     @Override
     public void addMessage() {
-         System.out.println("What would you like to say for the Morning Greeting of the Day?");
-        Scanner keyboard = new Scanner(System.in);
-        newMessage= keyboard.nextLine();
-        System.out.println(newMessage);
-    }
+Object[] options = {"Yes, please",
+                    "No way!"};
+int n = JOptionPane.showOptionDialog(frame,
+    "Would you like green eggs and ham?",
+    "A Silly Question",
+    JOptionPane.YES_NO_OPTION,
+    JOptionPane.QUESTION_MESSAGE,
+    null,     //do not use a custom Icon
+    options,  //the titles of buttons
+    options[0]); //default button title
 
-  
+if (n == JOptionPane.YES_OPTION) {
+          JOptionPane.showMessageDialog(null, "HELLO");
+        }
+        else {
+           JOptionPane.showMessageDialog(null, "GOODBYE");
+           System.exit(0);
+        }
+}
     
 }
